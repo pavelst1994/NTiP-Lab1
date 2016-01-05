9102c802addde855187145b775d904fbb1afb198
Павел Степанов <5822_spy@kcup.net>
2016-01-05 18:51:34 +0600

    Добавлены 2 новых класса для подсчета объема Пирамиды и Шара.

---
+++ ClassLibrary/Pyramid.cs
@@ -0,0 +1,22 @@
+﻿namespace Core
+{
+    public class Pyramid : IFigure
+    {
+        private int _square;
+        private int _heigth;
+        public Pyramid(int square, int heigth)
+        {
+            if ((square <= 0) || (heigth <= 0))
+            {
+                throw new ShapeExeption("Значение площади и высоты пирамиды должны быть положительными");
+            }
+
+            _square = square;
+            _heigth = heigth;//проверка
+        }
+        public double ReturnVolume()
+        {
+            return 1 / 3.0 * _square * _heigth;
+        }
+    }
+}
---
+++ ClassLibrary/Sphere.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Core
+{
+    public class Sphere : IFigure
+    {
+        private int _radius;
+        public Sphere(int radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ShapeExeption("Значение радиуса сферы должно быть положительным");
+            }
+            _radius = radius;//проверкана неотрицательнось
+        }
+        public double ReturnVolume()
+        {
+            return 4 / 3.0 * Math.PI * Math.Pow(_radius, 3);
+        }
+    }
+}
